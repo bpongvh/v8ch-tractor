0a190da6744f37a7863b53df5562d879bc4651c7
Robert Pratt <bpong@v8ch.com>
2018-07-02 16:56:44 +0700

    Enqueue block apps

--- app/controllers/front-page.php
+++ app/controllers/front-page.php
@@ -6,5 +6,12 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
-
+    public function __construct()
+    {
+        // Registered by V8CH Tractor Blocks plugin
+        wp_enqueue_script('v8ch_hero_app');
+        wp_enqueue_script('v8ch_projects_app');
+        wp_enqueue_script('v8ch_skills_app');
+        wp_enqueue_style('v8ch-tractor-blocks');
+    }
 }
