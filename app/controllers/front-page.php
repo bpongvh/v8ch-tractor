bc1c5280add7e6007d1309eaa7b9628cbd095785
Robert Pratt <bpong@v8ch.com>
2018-07-04 14:36:21 +0700

    Add contact

--- app/controllers/front-page.php
+++ app/controllers/front-page.php
@@ -9,6 +9,7 @@ class FrontPage extends Controller
     public function __construct()
     {
         // Registered by V8CH Tractor Blocks plugin
+        wp_enqueue_script('v8ch_contact_app');
         wp_enqueue_script('v8ch_hero_app');
         wp_enqueue_script('v8ch_projects_app');
         wp_enqueue_script('v8ch_skills_app');
